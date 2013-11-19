3b5bd9043f7b4c043075dca02050c640b81e613c
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2013-11-19 13:01:36 +0900

    Fully implement multiple sift files per object
    Conflicts:
    	blort_ros/src/gldetector.cpp

--- blort_ros/src/ObjectEntry.cpp
+++ blort_ros/src/ObjectEntry.cpp
@@ -2,6 +2,14 @@
 
 void blort_ros::buildFromFiles(const std::vector<std::string> & ply_models, const std::vector<std::string> & sift_files, const std::vector<std::string> & model_names, std::vector<blort_ros::ObjectEntry> & out)
 {
+    std::vector<size_t> sift_index(0);
+    return buildFromFiles(ply_models, sift_files, model_names, out, sift_index);
+}
+
+void blort_ros::buildFromFiles(const std::vector<std::string> & ply_models, const std::vector<std::string> & sift_files, const std::vector<std::string> & model_names, std::vector<blort_ros::ObjectEntry> & out, std::vector<size_t> & sift_index)
+{
+    out.resize(0);
+    sift_index.resize(0);
     for(size_t i = 0; i < model_names.size(); ++i)
     {
         blort_ros::ObjectEntry entry;
@@ -22,5 +30,14 @@ void blort_ros::buildFromFiles(const std::vector<std::string> & ply_models, cons
             }
         }
         out.push_back(entry);
+        if(i != 0)
+        {
+            sift_index.push_back(sift_index[i-1] + out[i-1].sift_files.size());
+        }
+        else
+        {
+            sift_index.push_back(0);
+        }
     }
+    sift_index.push_back(sift_index[sift_index.size()-1] + out[out.size()-1].sift_files.size());
 }
--- blort_ros/src/ObjectEntry.h
+++ blort_ros/src/ObjectEntry.h
@@ -14,7 +14,11 @@ struct ObjectEntry
     std::vector<std::string> sift_files;
 };
 
+/* This build a vector of ObjectEntry from ply_models/sift_files/model_names vector */
 void buildFromFiles(const std::vector<std::string> & ply_models, const std::vector<std::string> & sift_files, const std::vector<std::string> & model_names, std::vector<ObjectEntry> & out);
+
+/* Additionnaly build a sift-index */
+void buildFromFiles(const std::vector<std::string> & ply_models, const std::vector<std::string> & sift_files, const std::vector<std::string> & model_names, std::vector<ObjectEntry> & out, std::vector<size_t> & sift_index);
 
 } // namespace blort_ros
 
